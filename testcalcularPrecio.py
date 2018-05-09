d594ff39ef68f15c6914ba0c56d5089c4799d4a3
gnus19 <37775126+gnus19@users.noreply.github.com>
2018-05-09 08:27:15 -0400

    Creacion de testMinTiempoDeServicio
    Se debe solucionar testMinTiempoDeServicio

--- testcalcularPrecio.py
+++ testcalcularPrecio.py
@@ -16,10 +16,14 @@ class calcularPrecioTest(unittest.TestCase):
         self.tiempoF = datetime.strptime('Jun 01 2005 19:05', '%b %d %Y %H:%M')
         self.clase = calcularPrecio()
     
-    def testCero(self):
+    def testTiempoDeServicioCero(self):
         self.assertEquals(None, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
         
-
+    def testMinTiempoDeServicio(self):
+        self.tiempoI = datetime.strptime('Jun 01 2005 19:00', '%b %d %Y %H:%M')
+        self.tiempoF = datetime.strptime('Jun 01 2005 19:15', '%b %d %Y %H:%M')
+        self.assertEquals(5, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
