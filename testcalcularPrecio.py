661bc1483ac2c903118df637802fe144d4370ac2
Angelj413 <morante413@gmail.com>
2018-05-10 09:01:12 -0400

    Creación de test14Min, que pruea si el tiempo de servicio fue de 14 minutos. todo OK

--- testcalcularPrecio.py
+++ testcalcularPrecio.py
@@ -16,6 +16,7 @@ class calcularPrecioTest(unittest.TestCase):
         self.tiempoF = datetime.strptime('Jun 01 2005 19:05', '%b %d %Y %H:%M')
         self.clase = calcularPrecio()
     
+    #Esta prueba es equivalente a verificar si en la clase existe la funcion
     def testTiempoDeServicioCero(self):
         self.assertEquals(None, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
         
@@ -24,7 +25,12 @@ class calcularPrecioTest(unittest.TestCase):
         self.tiempoF = datetime.strptime('Jun 01 2005 19:15', '%b %d %Y %H:%M')
         self.assertEquals(5, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
         
+    def test14Min(self):
+        self.tiempoI = datetime.strptime('Jun 01 2005 19:00', '%b %d %Y %H:%M')
+        self.tiempoF = datetime.strptime('Jun 01 2005 19:14', '%b %d %Y %H:%M')
+        self.assertEquals(None, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
 
+        
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
