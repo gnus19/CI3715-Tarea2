a79b0a695b71310559347faa5ef66dc5d9ee6d24
Angelj413 <morante413@gmail.com>
2018-05-10 09:05:18 -0400

    Creación de test16Min, cuando el tiempo de servicio es de 16 minutos

--- testcalcularPrecio.py
+++ testcalcularPrecio.py
@@ -31,6 +31,10 @@ class calcularPrecioTest(unittest.TestCase):
         self.assertEquals(None, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
 
         
+    def test16Min(self):
+        self.tiempoI = datetime.strptime('Jun 01 2005 19:00', '%b %d %Y %H:%M')
+        self.tiempoF = datetime.strptime('Jun 01 2005 19:16', '%b %d %Y %H:%M')
+        self.assertEquals(5, self.clase.calcularPrecio(self.tarifa, [self.tiempoI, self.tiempoF]))
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
